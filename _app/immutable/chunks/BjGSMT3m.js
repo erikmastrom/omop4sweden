8ad0b257445ba4d05a5650800bab5f1a4bbad284
erikmastrom <erikmagnussonastrom@gmail.com>
2025-09-09 21:05:27 +0200

    Update site to v1.1

---
+++ _app/immutable/chunks/BjGSMT3m.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_w4bto5?.base??"/omop4sweden",e=globalThis.__sveltekit_w4bto5?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/CVj09I_B.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_hqmt3s?.base??"/omop4sweden",e=globalThis.__sveltekit_hqmt3s?.assets??s;export{e as a,s as b};
