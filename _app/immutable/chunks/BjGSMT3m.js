2324741f4e9081830320da2a2d8f2a57a642b224
erikmastrom <erikmagnussonastrom@gmail.com>
2025-09-09 21:49:48 +0200

    Update site to v1.2

--- _app/immutable/chunks/BjGSMT3m.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_w4bto5?.base??"/omop4sweden",e=globalThis.__sveltekit_w4bto5?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/D7zerlnd.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_a6yfte?.base??"/omop4sweden",e=globalThis.__sveltekit_a6yfte?.assets??s;export{e as a,s as b};
