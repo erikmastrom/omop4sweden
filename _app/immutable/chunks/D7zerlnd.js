713d0aaa00449e143dffe0efb1161024c287c4e1
erikmastrom <erikmagnussonastrom@gmail.com>
2025-10-02 18:07:58 +0200

    fixed errors, added abbvie, added event

--- _app/immutable/chunks/D7zerlnd.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_a6yfte?.base??"/omop4sweden",e=globalThis.__sveltekit_a6yfte?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/azc88JaH.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_fxm1mc?.base??"/omop4sweden",e=globalThis.__sveltekit_fxm1mc?.assets??s;export{e as a,s as b};
