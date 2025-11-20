28fe5d3f612ea55add41e78a7792a5588fb0172f
Erik M. Åström <erik@Eriks-MacBook-Air.local>
2025-11-20 17:22:46 +0100

    Added the symposium page and new events, as well as speaker cards and a list of the speakers. From new computer.

---
+++ _app/immutable/chunks/CQQc3Lq5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_5ro2mq?.base??"/omop4sweden",e=globalThis.__sveltekit_5ro2mq?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/azc88JaH.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_fxm1mc?.base??"/omop4sweden",e=globalThis.__sveltekit_fxm1mc?.assets??s;export{e as a,s as b};
