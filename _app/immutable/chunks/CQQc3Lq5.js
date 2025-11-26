eaa5eba8ea1451c3bc12061225d0ec7cc1069938
Erik M. Åström <erik@Eriks-MacBook-Air.local>
2025-11-26 18:47:52 +0100

    fixed pdf 404-error and fixed mobile view for symposium page

--- _app/immutable/chunks/CQQc3Lq5.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_5ro2mq?.base??"/omop4sweden",e=globalThis.__sveltekit_5ro2mq?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/CmVOuxPZ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_jkfitx?.base??"/omop4sweden",e=globalThis.__sveltekit_jkfitx?.assets??s;export{e as a,s as b};
