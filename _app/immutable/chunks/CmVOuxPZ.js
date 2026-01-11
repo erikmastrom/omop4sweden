342e5a3989e88410d2715c2c464d315dc1982672
Erik M. Åström <erik@Eriks-MacBook-Air.local>
2026-01-11 16:55:05 +0100

    added work package 4 and 5, added Jake, added The Intelligent Systems Laboratory

--- _app/immutable/chunks/CmVOuxPZ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jkfitx?.base??"/omop4sweden",e=globalThis.__sveltekit_jkfitx?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/DxRyb1RD.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1536uyk?.base??"/omop4sweden",e=globalThis.__sveltekit_1536uyk?.assets??s;export{e as a,s as b};
