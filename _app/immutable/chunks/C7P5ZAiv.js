0c1ca3ebeb515ea1228d3393c2c073dd398eb742
Erik Åström <erikmagnussonastrom@gmail.com>
2026-01-20 23:14:21 +0100

    added OMOP Seminar Series event and fixed title

---
+++ _app/immutable/chunks/C7P5ZAiv.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_3pz46u?.base??"/omop4sweden",e=globalThis.__sveltekit_3pz46u?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/CxdERwbJ.js
@@ -0,0 +1,1 @@
+import{a5 as a}from"./Bx7znH4Q.js";a();
