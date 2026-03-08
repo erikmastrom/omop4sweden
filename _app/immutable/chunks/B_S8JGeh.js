ce6a46a852c8143eb9ec6d177974884a110926f9
Erik Åström <erikmagnussonastrom@gmail.com>
2026-03-08 23:19:48 +0100

    edited OMOP School page and added hover effects to the accordions

--- _app/immutable/chunks/B_S8JGeh.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_z9mzx3?.base??"/omop4sweden",e=globalThis.__sveltekit_z9mzx3?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/JGxs0fnW.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_81y95o?.base??"/omop4sweden",e=globalThis.__sveltekit_81y95o?.assets??s;export{e as a,s as b};
