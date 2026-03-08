f031e8e751f8dfa4cc3f7e206e1193763eea1f0d
Erik Åström <erikmagnussonastrom@gmail.com>
2026-03-08 19:26:01 +0100

    added OMOP School page

--- _app/immutable/chunks/CRbruJ-j.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_u943ko?.base??"/omop4sweden",e=globalThis.__sveltekit_u943ko?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/CxdERwbJ.js
+++
@@ -1,1 +0,0 @@
-import{a5 as a}from"./Bx7znH4Q.js";a();
