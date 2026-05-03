0a479f009b06c825c1d886b152f99c3b088250c5
Erik Åström <erikmagnussonastrom@gmail.com>
2026-05-03 19:33:48 +0200

    changed picture and description of Johanna

--- _app/immutable/chunks/D06jPJWF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_i07bmb?.base??"/omop4sweden",e=globalThis.__sveltekit_i07bmb?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/D_sflCrY.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_lohy1?.base??"/omop4sweden",e=globalThis.__sveltekit_lohy1?.assets??s;export{e as a,s as b};
