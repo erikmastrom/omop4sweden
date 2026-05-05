3ba2f43f881312c957a421299473526c48c4337b
Erik Åström <erikmagnussonastrom@gmail.com>
2026-05-05 09:45:23 +0200

    resolved typo i last commit

---
+++ _app/immutable/chunks/___xRfLT.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xjmlng?.base??"/omop4sweden",e=globalThis.__sveltekit_xjmlng?.assets??s;export{e as a,s as b};
--- _app/immutable/entry/start.CDLxfMWu.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/XzPUlNpf.js";export{o as load_css,r as start};
