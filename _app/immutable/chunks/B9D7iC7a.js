d323e8e28a4768063372b86a4f3ef5c2048efe40
Erik Åström <erikmagnussonastrom@gmail.com>
2026-07-12 17:45:28 +0200

    changed fontsize of swedish hero title

--- _app/immutable/chunks/B9D7iC7a.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jm6ymr?.base??"/omop4sweden",e=globalThis.__sveltekit_jm6ymr?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/BUvUdpwE.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_g2ndrs?.base??"/omop4sweden",e=globalThis.__sveltekit_g2ndrs?.assets??s;export{e as a,s as b};
